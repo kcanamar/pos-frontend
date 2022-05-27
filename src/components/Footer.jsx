cb03d032df2155dcf10f8dbb2545f9543593d7bc
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-27 15:17:45 -0700

    updates footer and populates menu page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,26 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import HomeIcon from '@mui/icons-material/Home';
 import MenuIcon from '@mui/icons-material/Menu';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Paper from '@mui/material/Paper'
 
 export default function Footer() {
   const [value, setValue] = React.useState(0);
 
   return (
     <Box sx={{ width: 375 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-        <BottomNavigationAction label="Menu" icon={<MenuIcon />} />
-        <BottomNavigationAction label="Admin" icon={<LocationOnIcon />} />
-      </BottomNavigation>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+          <BottomNavigationAction label="Menu" icon={<MenuIcon />} />
+          <BottomNavigationAction label="Admin" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Paper>
     </Box>
   );
 }
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,3 +1,16 @@
-export default function Menu() {
-    return <h1>Menu</h1>
+export default function Menu({ foods }) {
+
+    const loaded = () => {
+        return foods.map((item) => (
+            <div key={item._id} className="item">
+                    <h3>{item.PosName}</h3>
+                    <img src={item.image} alt={item.name}/>
+                    <h5>{item.price}</h5>
+            </div>
+        ))
+    }
+
+    const loading = () => <h1>Loading....</h1>
+
+    return foods ? loaded() : loading()
 }
