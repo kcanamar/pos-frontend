757e121c52f41c75c966a1859f594b7a0a07cef0
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-28 17:00:18 -0700

    styles admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,6 +2,32 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import { Grid, Paper } from "@mui/material"
 
+const createStyle = {
+    marginBottom: 5
+}
+
+const formStyle = {
+    marginBottom: 20,
+}
+const inputStyle = {
+    padding: 5,
+    margin: 5,
+    borderRadius: "5px",
+}
+
+const submitStyle = {
+    backgroundColor: "green",
+    color: "white",
+    padding: 5,
+    margin: 5,
+    borderRadius: "5px",
+
+}
+
+const linkStyle = {
+    textDecoration: "none",
+    color: "black"
+}
 export default function Admin({ foods, createFood, updateFood, deleteFood }) {
 
     const [newForm, setNewForm] = useState({
@@ -35,9 +61,9 @@ export default function Admin({ foods, createFood, updateFood, deleteFood }) {
         return <Grid container spacing={2}>
             {foods.map((item) => (
                 <Grid item xs={4} key={item._id} >
-                    <Link to={`/admin/${item._id}`}>
-                        <Paper>
-                            <h3>{item.PosName}</h3>
+                    <Link style={linkStyle} to={`/admin/${item._id}`}>
+                        <Paper sx={{ textAlign: "center", height: "75px", display: "flex", justifyContent: "center", marginTop: 0}}>
+                            <h3 style={{ marginTop: 13}}>{item.PosName}</h3>
                         </Paper>
                     </Link>
                 </Grid>
@@ -49,12 +75,14 @@ export default function Admin({ foods, createFood, updateFood, deleteFood }) {
 
     return (
         <div>
-            <form onSubmit={handleNewSubmit}>
-                <input type="text" value={newForm.name} name="name" placeholder="name" onChange={handleNewChange} required />
-                <input type="text" value={newForm.PosName} name="PosName" placeholder="PosName" onChange={handleNewChange} required />
-                <input type="text" value={newForm.image} name="image" placeholder="image URL" onChange={handleNewChange}/>
-                <input type="text" value={newForm.price} name="price" placeholder="price" onChange={handleNewChange} required />
-                <input type="submit" value="Create New Menu Item"/>
+            <h1>Admin DashBoard</h1>
+            <h3 style={createStyle}>Create New Item</h3>
+            <form style={formStyle} onSubmit={handleNewSubmit}>
+                <input style={inputStyle}type="text" value={newForm.name} name="name" placeholder="name" onChange={handleNewChange} required />
+                <input style={inputStyle}type="text" value={newForm.PosName} name="PosName" placeholder="PosName" onChange={handleNewChange} required />
+                <input style={inputStyle}type="text" value={newForm.image} name="image" placeholder="image URL" onChange={handleNewChange}/>
+                <input style={inputStyle}type="text" value={newForm.price} name="price" placeholder="price" onChange={handleNewChange} required />
+                <input style={submitStyle}type="submit" value="Create New Menu Item"/>
             </form>
             {foods ? loaded() : loading()}
         </div>
