54b294400da044cb24098190bf2c983508de5c30
Kyle Canamar <canamar.kyle@gmail.com>
2022-05-28 17:33:19 -0700

    styles adimn edit page

--- src/pages/AdminEdit.jsx
+++ src/pages/AdminEdit.jsx
@@ -1,7 +1,33 @@
 import { useState } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 
-export default function Show({ foods, updateFood, deleteFood }) {
+const formStyle = {
+    marginBottom: 20,
+}
+const inputStyle = {
+    padding: 15,
+    margin: 5,
+    borderRadius: "5px",
+    fontSize: "24px",
+}
+
+const submitStyle = {
+    backgroundColor: "green",
+    color: "white",
+    padding: 15,
+    margin: 5,
+    borderRadius: "5px",
+}
+
+const deleteStyle = {
+    backgroundColor: "red",
+    color: "white",
+    padding: 15,
+    margin: 5,
+    borderRadius: "5px",
+}
+
+export default function AdminEdit({ foods, updateFood, deleteFood }) {
     const { id } = useParams();
     const item = foods.find((i) => i._id === id) 
     let navigate = useNavigate()
@@ -26,17 +52,17 @@ export default function Show({ foods, updateFood, deleteFood }) {
         navigate("/admin")
     }
     return (
-        <div className="item-edit">
+        <div >
             <h1>{item.name}</h1>
-            <button id="delete" onClick={removeFood}>
-                DELETE Item
+            <button style={deleteStyle}id="delete" onClick={removeFood}>
+                DELETE 
             </button>
-            <form onSubmit={handleSubmit}>
-                <input type="text" value={editForm.name} name="name" placeholder="name" onChange={handleChange}/>
-                <input type="text" value={editForm.PosName} name="PosName" placeholder="PosName" onChange={handleChange}/>
-                <input type="text" value={editForm.image} name="image" placeholder="image" onChange={handleChange}/>
-                <input type="text" value={editForm.price} name="price" placeholder="price" onChange={handleChange}/>
-                <input type="submit" value="Update Item"/>
+            <form style={formStyle} onSubmit={handleSubmit}>
+                <input style={inputStyle} type="text" value={editForm.name} name="name" placeholder="name" onChange={handleChange}/> <br/>
+                <input style={inputStyle} type="text" value={editForm.PosName} name="PosName" placeholder="PosName" onChange={handleChange}/> <br/>
+                <input style={inputStyle} type="text" value={editForm.image} name="image" placeholder="image" onChange={handleChange}/> <br/>
+                <input style={inputStyle} type="text" value={editForm.price} name="price" placeholder="price" onChange={handleChange}/> <br/>
+                <input style={submitStyle} type="submit" value="Update Item"/>
             </form>
         </div>
     )
